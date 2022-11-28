a4b2ad23e5b6e5f400f28de8fef627f863d5cfdc
szhan253 <szhan253@ucsc.edu>
2022-11-28 01:58:25 -0800

    get basic hit an object reduce or apply health
    but have no idea why the tile object gonna disappear in reduce health

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    public float maxHealth;
+    public float currentHealth;
+ 
+    public float damage;
+    public float healing;
+
+    public GameObject reduceHP;
+    public GameObject applyHP;
+ 
+ 
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        reduceHP = GameObject.Find("ReduceHealth");
+        applyHP = GameObject.Find("ApplyHealth");
+    }
+ 
+ 
+     public void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        if (collisionInfo.collider.name == "ApplyHealth")
+        {
+            ApplyHealing();
+            Destroy(applyHP);
+        }
+ 
+        if(collisionInfo.collider.name == "ReduceHealth")
+        {
+            DamageHealth();
+            Destroy(reduceHP);
+        }
+
+        Debug.Log("Current Health: " + currentHealth);
+    }
+ 
+    void ApplyHealing()
+    {
+        if (currentHealth < maxHealth)
+        {
+            currentHealth = currentHealth + healing;
+        }
+    }
+ 
+    public void DamageHealth()
+    {
+        if (currentHealth > 0)
+        {
+            currentHealth = currentHealth - damage;
+        }
+    }
+}
